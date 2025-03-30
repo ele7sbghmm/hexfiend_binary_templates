15afc1377ddc5e6d7ff0c2cabadb695df12b5bb1
m <im@ms-iMac.local>
2025-03-30 19:42:51 +1300

    luaP code

--- amped2/pgf.tcl
+++ amped2/pgf.tcl
@@ -3,31 +3,22 @@ little_endian
 float "version"
 hex 4 "-"
 
-proc Headers {n} {
-  for {set i 0} {$i < $n} {incr i} {
-    #move 4; uint32 "$i "; move 12
-    move 20
-  }
-}
-
-section "PgfSizes" {
-  set totalFileSize   [uint32 "TotalFileSize"]
+section -collapsed "PgfSizes" {
+  uint32 "TotalFileSize"
   set totalDataSize   [uint32 "TotalDataSize"]
   set textureDataSize [uint32 "TextureDataSize"]
   set numTextures     [uint32 "NumTextures"]
   set shaderDataSize  [uint32 "ShaderDataSize"]
 }
-
 hex 4 "-"
 
-#Headers $NumTextures
 bytes [expr $numTextures * 20]
-set textureBuffer [bytes $textureDataSize "TextureBuffer"]
+bytes $textureDataSize "TextureBuffer"
 hex 4 "-"
-set shaderBuffer [bytes $shaderDataSize "ShaderBuffer"]
+bytes $shaderDataSize "ShaderBuffer"
 hex 4 "-"
 
-section "PgfHeader" {
+section -collapsed "PgfHeader" {
   set vbDataSize        [uint32 "VBDataSize"]
   set ibDataSize        [uint32 "IBDataSize"]
   set pbDataSize        [uint32 "PBDataSize"]
@@ -47,7 +38,8 @@ section "PgfHeader" {
 }
 
 hex 4 "-"
-dict set CFileManager vertexData    [bytes $vbDataSize "Vertex Data"]
+#dict set CFileManager vertexData    [bytes $vbDataSize "Vertex Data"]
+bytes $vbDataSize "vertex data"
 set vertexHeaders [bytes [expr $numVertexBuffers * 12] "Vertex Headers"]
 #set vertexHeaders a
 #for {set i 0} {$i < $numVertexBuffers} {incr i} {
@@ -55,17 +47,23 @@ set vertexHeaders [bytes [expr $numVertexBuffers * 12] "Vertex Headers"]
 #}
 
 set indexData    [bytes $ibDataSize "Index Data"]
-set indexHeaders [bytes [expr $numIndexBuffers * 12] "Index Headers"]
-#for {set i 0} {$i < $numIndexBuffers} {incr i} { uint32; uint32 "ib$i"; uint32 }
+#set indexHeaders [bytes [expr $numIndexBuffers * 12] "Index Headers"]
+#set indexHeaders [list [repeat $numIndexBuffers [section -collapse "" { uint32; uint32 "offset: "; uint32 }]]]
+set indexHeaders [list]
+section -collapsed "index headers" {
+  for {set i 0} {$i < $numIndexBuffers} {incr i} {
+    uint32; lappend $indexHeaders [uint32 "$i"]; uint32
+  }
+}
 
 hex 4 "-"
-#set pushBufferData     [bytes $numPushBuffers "PushBufferData"]
-set boundingVolumeData [bytes $bvDataSize "Bounding Volume Data"]
-set miscData           [bytes $miscDataSize "Misc Data"]
-#set influenceData      [bytes $influenceDataSize "Influence Data"]
-#set limData            [bytes $limDataSize "LIM Data"]
-set collisionData      [bytes $collisionDataSize "Collision Data"]
-set stringTable        [bytes $stringTableSize "String Table"]
-set primList           [bytes [expr $numPrimLists * 48] "Prim List"]
-set vbGeomData         [bytes [expr $numVBGeomData * 48] "VB Geom Data List"]
+if { $numPushBuffers }    { set pushBufferData     [bytes $numPushBuffers "PushBufferData"] }
+if { $bvDataSize }        { set boundingVolumeData [bytes $bvDataSize "Bounding Volume Data"] }
+if { $miscDataSize }      { set miscData           [bytes $miscDataSize "Misc Data"] }
+if { $influenceDataSize } { set influenceData      [bytes $influenceDataSize "Influence Data"] }
+if { $limDataSize }       { set limData            [bytes $limDataSize "LIM Data"] }
+if { $collisionDataSize } { set collisionData      [bytes $collisionDataSize "Collision Data"] }
+if { $stringTableSize }   { set stringTable        [bytes $stringTableSize "String Table"] }
+if { $numPrimLists }      { set primList           [bytes [expr $numPrimLists * 48] "Prim List"] }
+if { $numVBGeomData }     { set vbGeomData         [bytes [expr $numVBGeomData * 48] "vb geom data list"] }
 
